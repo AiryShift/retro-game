40e26b4de3ba4668c2652c34711115945e0d3718
AiryShift <religionofaperturelabs@gmail.com>
2015-07-21 13:24:39 +1000

    Add more forms for the different screens

--- pacman/My Project/Application.Designer.vb
+++ pacman/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.pacman.Form1
+            Me.MainForm = Global.pacman.game
         End Sub
     End Class
 End Namespace
