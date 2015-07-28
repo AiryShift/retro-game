694ef6aa5fd31808f41d055a96cadba21e5979d8
AiryShift <religionofaperturelabs@gmail.com>
2015-07-28 12:45:40 +1000

    Create the ship picturebox

--- roids/game.Designer.vb
+++ roids/game.Designer.vb
@@ -22,17 +22,31 @@ Partial Class game
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.ship = New System.Windows.Forms.PictureBox()
+        CType(Me.ship, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
-        'Form1
+        'ship
+        '
+        Me.ship.BackColor = System.Drawing.SystemColors.ControlDarkDark
+        Me.ship.Location = New System.Drawing.Point(337, 317)
+        Me.ship.Name = "ship"
+        Me.ship.Size = New System.Drawing.Size(30, 30)
+        Me.ship.TabIndex = 0
+        Me.ship.TabStop = False
+        '
+        'game
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(584, 562)
-        Me.Name = "Form1"
+        Me.ClientSize = New System.Drawing.Size(684, 662)
+        Me.Controls.Add(Me.ship)
+        Me.Name = "game"
         Me.Text = "Form1"
+        CType(Me.ship, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
+    Friend WithEvents ship As System.Windows.Forms.PictureBox
 
 End Class
