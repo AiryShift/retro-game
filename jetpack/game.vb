89403f91341e3a0d1165e66db5cd41187c5b0d7f
AiryShift <religionofaperturelabs@gmail.com>
2015-07-30 12:20:48 +1000

    Try again with jetpack

---
+++ jetpack/game.vb
@@ -0,0 +1,18 @@
+﻿Public Class game
+    Dim WithEvents tick As New Timer
+
+    Public Class sprite
+        Dim yVector As Decimal = 0
+        Dim xVector As Decimal = 0
+    End Class
+
+    Private Class player : Inherits sprite
+    End Class
+
+    Private Class enemy : Inherits sprite
+    End Class
+
+    Private Sub init() Handles Me.Activated
+
+    End Sub
+End Class
