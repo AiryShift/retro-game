4f16121d4da5b83fbc745e98fec47e6b46c60893
AiryShift <religionofaperturelabs@gmail.com>
2015-08-06 12:03:50 +1000

    Change things

--- jetpack/game.vb
+++ jetpack/game.vb
@@ -1,34 +1,29 @@
 ﻿Public Class game
-    Dim locationasd As New Point(0, 0)
+    Dim drawing As Sprite()
 
-    Public Sub draw(ByVal e As PaintEventArgs, img As Image, coords As Point)
-        ' Create rectangle for displaying image.
-        Dim destRect As New Rectangle(coords.X, coords.Y, img.Width, img.Height)
+    Public Class Sprite
+        Public coord As Point
+        Public img As Bitmap
+    End Class
 
-        ' Draw image to screen.
-        e.Graphics.DrawImage(img, destRect)
+    Public Sub draw(ByVal sender As Object, ByVal e As PaintEventArgs) Handles MyBase.Paint
+        For Each i In drawing
+            ' Create rectangle for displaying image.
+            Dim destRect As New Rectangle(i.coord.X, i.coord.Y, i.img.Width, i.img.Height)
+
+            ' Draw image to screen.
+            e.Graphics.DrawImage(i.img, destRect)
+        Next
     End Sub
 
-
-    Private Sub init() Handles Me.Activated
-
-    End Sub
-
-    Private Sub clearScreen(sender As System.Object, e As System.EventArgs) Handles universalTick.Tick
+    Private Sub screenUpdate(sender As System.Object, e As System.EventArgs) Handles universalTick.Tick
         Me.Invalidate()
     End Sub
-End Class
 
-Public Class SpriteMaster
-    Dim currentPosition As New Point(0, 0)
-    Dim xVector As Decimal = 0
-    Dim yVector As Decimal = 0
-End Class
-
-Public Class player : Inherits SpriteMaster
-    Dim character As Graphics = Graphics.FromImage(My.Resources.mario)
-    draw(character, currentPosition)
-End Class
-
-Public Class enemy : Inherits SpriteMaster
+    Private Sub init() Handles Me.Activated
+        Dim player As New Sprite
+        player.coord.X = 0
+        player.coord.Y = 0
+        player.img = My.Resources.mario
+    End Sub
 End Class
