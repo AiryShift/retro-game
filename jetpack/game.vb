cb9e53c87d27cc616ded689c86db37c529589a4d
AiryShift <religionofaperturelabs@gmail.com>
2015-08-20 12:13:58 +1000

    Update style

--- jetpack/game.vb
+++ jetpack/game.vb
@@ -1,7 +1,8 @@
 ﻿Public Class game
     Dim render As New List(Of Sprite)
     Public Declare Function GetAsyncKeyState Lib "user32.dll" (ByVal vKey As Int32) As UShort
-    Structure Direction
+
+    Structure Velocity
         Dim X As Decimal
         Dim Y As Decimal
     End Structure
@@ -9,7 +10,8 @@
     Structure Sprite
         Dim coord As Point
         Dim img As Image
-        Dim dir As Direction
+        Dim vel As Velocity
+        Dim id As String
     End Structure
 
     Private Sub updateSprites(sender As System.Object, e As System.EventArgs) Handles universalTick.Tick
@@ -22,11 +24,19 @@
         Next
     End Sub
 
+    Private Function initSprite(img As Image, x As Integer, y As Integer, id As String, Optional xV As Decimal = 0, Optional yV As Decimal = 0)
+        Dim a As New Sprite
+        a.coord.X = x
+        a.coord.Y = y
+        a.img = img
+        a.id = id
+        a.vel.X = xV
+        a.vel.Y = yV
+        Return a
+    End Function
+
     Private Sub init() Handles Me.GotFocus
-        Dim player As New Sprite
-        player.coord.X = 200
-        player.coord.Y = 200
-        player.img = My.Resources.test
+        Dim player = initSprite(My.Resources.test, 200, 200, "PLAYER")
         render.Add(player)
         universalTick.Enabled = True
     End Sub
