f329b43fd5e4339705499c3d32cd2f02a183b78b
AiryShift <religionofaperturelabs@gmail.com>
2015-08-21 11:56:23 +1000

    Do more things

--- jetpack/game.vb
+++ jetpack/game.vb
@@ -2,25 +2,42 @@
     Dim render As New List(Of Sprite)
     Public Declare Function GetAsyncKeyState Lib "user32.dll" (ByVal vKey As Int32) As UShort
 
-    Structure Velocity
-        Dim X As Decimal
-        Dim Y As Decimal
-    End Structure
+    Class Velocity
+        Public X As Decimal
+        Public Y As Decimal
+    End Class
 
-    Structure Sprite
-        Dim coord As Point
-        Dim img As Image
-        Dim vel As Velocity
-        Dim id As String
-    End Structure
+    Class Sprite
+        Public coord As Point
+        Public img As Image
+        Public vel As Velocity
+        Public id As String
+    End Class
 
     Private Sub updateSprites(sender As System.Object, e As System.EventArgs) Handles universalTick.Tick
+        For i As Integer = 0 To render.Count()
+            Dim obj = render(i)
+
+            If obj.id = "PLAYER" Then
+            End If
+            'gravity
+            If True Then ' TODO: above ground
+                obj.vel.Y -= 0.2
+            End If
+
+            render(i) = obj
+        Next
         Me.Invalidate()
     End Sub
 
     Private Sub draw(ByVal sender As Object, ByVal e As System.Windows.Forms.PaintEventArgs) Handles MyBase.Paint
-        For Each i As Sprite In render
-            e.Graphics.DrawImage(i.img, i.coord.X, i.coord.Y)
+        For i As Integer = 0 To render.Count()
+            Dim obj = render(i)
+            'update positions
+            obj.coord.X += obj.vel.X
+            obj.coord.Y += obj.vel.Y
+            e.Graphics.DrawImage(obj.img, obj.coord.X, obj.coord.Y)
+            render(i) = obj
         Next
     End Sub
 
